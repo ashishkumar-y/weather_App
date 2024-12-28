46354d2f51d40a2233210bb73f01446b0bfb50db
Ashish Kumar <ashishkumar@Sumits-MacBook-Air.local>
2024-12-28 18:29:04 +0530

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import Weather from './components/Weather'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -8,7 +8,6 @@ function App() {
       <div className="fixed inset-0 bg-fixed bg-cover bg-img" aria-label="Background image"></div>
       <main className="relative  z-1   items-center justify-center ">
         <div className=" w-full border h-screen pb-0 overflow-hidden p-10 ">
-    
           <Weather />
         </div>
       </main>
